4099c1c4d8ad8dd69d000f57799f98d45093dbed
Paul <paul-tang@astra.cloud>
2018-02-24 03:41:49 +0800

    [Modify] change 10 to 49

--- make_data.py
+++ make_data.py
@@ -25,7 +25,7 @@ def make_winner_num_dict(winner_num):
     winner_num_dict = dict()
     for num in winner_num:
         winner_num_dict[num] = 1
-        
+
     return winner_num_dict
 
 def dict_init(overall_dict, number):
@@ -41,22 +41,22 @@ def analyse_zero(base_array, target_input, overall_dict, len_array):
             if overall_dict.get(base_array[num]) is None:
                 overall_dict[base_array[num]] = 1
             else:
-                overall_dict[base_array[num]] += 1            
+                overall_dict[base_array[num]] += 1
             base_array[num] = 0
         else:
             base_array[num] += 1
         num += 1
-    
+
     overall_dict.pop(0,None)
 #     print(base_array)
 #     print(overall_dict)
-        
+
 
 def make_winner_dict_list(winner_number):
     winner_num_dict = []
     for ii in winner_number:
         winner_num_dict.append(make_winner_num_dict(ii))
-    
+
     return winner_num_dict
 
 def make_numpy_file(winner_number_dict, start_name, odd, even):
@@ -70,33 +70,33 @@ def make_numpy_file(winner_number_dict, start_name, odd, even):
             sys.exit(0)
     # calculate every combination
 
-    for ii in tqdm(range(0, len(winner_number_dict))):
+    for ii in (range(0, len(winner_number_dict))):
         file_name = str(ii+int(start_name)) + '.data'
 #         print('計算檔案: ' + file_name + ' 中...')
         # combination_data = np.array([False]*com_num,np.bool)
         combination_data = [False] * com_num
         flag = 0
-        for i in odd:
+        for i in tqdm(odd):
             for j in even:
                 combination_data[flag] = compare2(i, j, winner_number_dict[ii])
-                flag += 1            
+                flag += 1
 #         print('製作檔案 ' + file_name + '中.....')
         try:
             f = open(os.path.join('data',file_name),'wb')
             combination_data_np = np.array(combination_data)
             # print(combination_data)
-            np.save(f,combination_data_np)            
-#             print('存檔，檔名為: ' + file_name + '，存檔成功')  
+            np.save(f,combination_data_np)
+#             print('存檔，檔名為: ' + file_name + '，存檔成功')
         except:
             print('[錯誤3] 存檔錯誤，跳過本檔案，請重新操作')
             sys.exit(0)
-        
+
 def get_winner_number_from_file(file_name):
     try:
         f2 = open(file_name, 'r')
         tmp = f2.read().split('\n')
         tmp = tmp[:len(tmp)-1]
-        winner_number = [[int(j) for j in (i.split(','))] for i in tmp] 
+        winner_number = [[int(j) for j in (i.split(','))] for i in tmp]
     except:
         print('[錯誤1] 操作異常，輸入檔名或是檔案有誤!')
         print('請重新操作')
@@ -113,13 +113,13 @@ def get_winner_number_from_input():
         print('請重新操作')
         sys.exit(0)
     return winner_number
-    
+
 
 def mode_1():
     # odd = list(iter.combinations(range(1,50,2),4))
     # even = list(iter.combinations(range(2,49,2),4))
-    odd = list(iter.combinations(range(1,10,2),4))
-    even = list(iter.combinations(range(2,10,2),4))        
+    odd = list(iter.combinations(range(1,50,2),4))
+    even = list(iter.combinations(range(2,49,2),4))
     input_file_name = input('請輸入包含正確格式的開獎號碼檔案名稱: ')
     print('開啟檔案 ' + input_file_name + " ......")
     winner_number = get_winner_number_from_file(input_file_name)
@@ -132,10 +132,11 @@ def mode_1():
     make_numpy_file(winner_number_dict_list, start_name, odd, even)
     time2 = time.clock() - time0
     print('本次計算時間: ' + str(time2) + ' 秒')
+    ex = input('請按Enter結束')
 
 def mode_2():
-    odd = list(iter.combinations(range(1,10,2),4))
-    even = list(iter.combinations(range(2,10,2),4))     
+    odd = list(iter.combinations(range(1,50,2),4))
+    even = list(iter.combinations(range(2,49,2),4))
     winner_number = get_winner_number_from_input()
     winner_number_dict_list = make_winner_dict_list(winner_number)
     start_name = input('請輸入本次輸入的期數: ')
@@ -143,6 +144,7 @@ def mode_2():
     make_numpy_file(winner_number_dict_list, start_name, odd, even)
     time2 = time.clock() - time0
     print('本次計算時間: ' + str(time2) + ' 秒')
+    ex = input('請按Enter結束')
 
 
 
@@ -155,7 +157,7 @@ def main():
         mode_2()
     else:
         print("輸入錯誤，請重新執行程式！")
-    
+
 
     # print('Welcome to the lottery combination system')
     # mode = input('Please enter the calculation mode: ')
@@ -164,11 +166,11 @@ def main():
 
     #     odd = list(iter.combinations(range(1,50,2),4))
     #     even = list(iter.combinations(range(2,49,2),4))
-    #     winner_number_raw = input('Please input the winner number: \n').split(',') # make string array to int 
+    #     winner_number_raw = input('Please input the winner number: \n').split(',') # make string array to int
     #     file_name = input('Plase enter the file name: ')
     #     winner_number = [int(num) for num in winner_number_raw]
 
-    #     winner_number_dict = make_dict(winner_number)   # make winner number bucket 
+    #     winner_number_dict = make_dict(winner_number)   # make winner number bucket
 
     #     # making combination test of odd and even
     #     time0 = time.clock()
@@ -195,7 +197,7 @@ def main():
     #     # odd = list(iter.combinations(range(1,50,2),4))
     #     # even = list(iter.combinations(range(2,49,2),4))
     #     odd = list(iter.combinations(range(1,10,2),4))
-    #     even = list(iter.combinations(range(2,11,2),4))        
+    #     even = list(iter.combinations(range(2,11,2),4))
     #     input_file_name = input('Please input the file including lottery number: ')
     #     print('Open the file ' + input_file_name + " ......")
     #     start_name = input('Please input the start file number: ')
@@ -209,7 +211,7 @@ def main():
     #     winner_number_dict = []
     #     for ii in winner_number:
     #         winner_number_dict.append(make_dict(ii))
-        
+
     #     # making combination test of odd and even
     #     time0 = time.clock()
     #     com_num = len(odd)*len(even)
@@ -226,16 +228,16 @@ def main():
     #         for i in odd:
     #             for j in even:
     #                 combination_data[flag] = compare2(i, j, winner_number_dict[ii])
-    #                 flag += 1            
+    #                 flag += 1
 
     #         print('Create file ' + file_name + '.....')
     #         f = open(file_name,'wb')
     #         combination_data_np = np.array(combination_data)
-    #         np.save(f,combination_data_np)            
+    #         np.save(f,combination_data_np)
 
     #         print('Save file ' + file_name + ' complete!')
 
-        
+
     #     time2 = time.clock() - time0
     #     print('The time elasped: ' + str(time2) + ' seconds')
 
@@ -267,7 +269,7 @@ def main():
     #         target_input = (np.load(f_read)).tolist()
     #         analyse_zero(base_array, target_input, overall_dict, com_num)
     #         f_read.close()
-            
+
     #     last_win_info = base_array.copy()
     #     analyse_zero(base_array, end_array, overall_dict, com_num)
     #     overall_time = time.clock() - start_time
@@ -278,7 +280,7 @@ def main():
     #     f_last_save = open(str(load_file_name_first) + "_" + str(load_file_name_last) + "last_only.analyse",'wb' )
     #     np.save(f_last_save,np.array(base_array,np.int16) - np.array(last_win_info,np.int16))
     #     print("Save file OK")
-            
+
     # elif mode == '4':
     #     load_old_overall_dict = input('Please enter the analyse file that you want to append:')
     #     load_file_name_first = int(load_old_overall_dict.split('_')[0])
